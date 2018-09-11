ca1a3ae3e55a884c72fba60208fcb7a0007a4840
Lars Doucet <lars.doucet@gmail.com>
2018-09-11 15:45:07 -0500

    add support for polymodpacks, requires new initialization signature

--- polymod/Polymod.hx
+++ polymod/Polymod.hx
@@ -16,14 +16,13 @@ class Polymod
 	private static var modLibrary:ModAssetLibrary = null;
 	/**
 	 * Initializes the chosen mod or mods.
-	 * @param	dir	root directory of a single mod
-	 * @param	dirs	root directories of several mods, in the intended loading order. <code>dir</code> is ignored if this is defined as well.
+	 * @param	modRoot	root directory of all mods
+	 * @param	dirs	directory names of one or more mods, relative to modRoot
 	 */
-	public static function init(?dir:String, ?dirs:Array<String>)
+	public static function init(modRoot:String, dirs:Array<String>)
 	{
-		var dirNull = dir == null || dir == "";
-		var dirsNull = dirs == null || dirs.length == 0;
-		if (dirNull && dirsNull)
+		trace("Polymod.init("+modRoot+","+dirs+")");
+		if (modRoot == null || dirs == null || dirs.length == 0)
 		{
 			if (defaultLibrary != null)
 			{
@@ -43,7 +42,32 @@ class Polymod
 		clearCache();
 		
 		modLibrary = new ModAssetLibrary(dir, defaultLibrary, dirs);
+		for(i in 0...dirs.length) {
+			if(dirs[i] != null){
+				dirs[i] = modRoot + "/" + dirs[i];
+			}
+		}
+		modLibrary = new ModAssetLibrary(null, defaultLibrary, dirs);
 		LimeAssets.registerLibrary("default", modLibrary);
+
+		if(Assets.exists("_polymodpack.txt"))
+		{
+			initModPack(modRoot);
+		}
+	}
+
+	private static function initModPack(modRoot:String)
+	{
+		var polymodpack:String = Assets.getText("_polymodpack.txt");
+		if(polymodpack != null)
+		{
+			var mods = polymodpack.split(",");
+			if(mods == null || mods.length == 0)
+			{
+				return;
+			}
+			init(modRoot, mods);
+		}
 	}
 	
 	private static function clearCache()
