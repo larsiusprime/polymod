d4c4883abb6d750a8ea0898a345a3a80f705dd46
Lars Doucet <lars.doucet@gmail.com>
2018-09-25 16:57:54 -0500

    backend cleanup

--- polymod/backends/IBackend.hx
+++ polymod/backends/IBackend.hx
@@ -2,17 +2,37 @@ package polymod.backends;
 
 interface IBackend
 {
+    //Synchronous asset fetch from modified set:
     public function getText(id:String):String;
     public function getBytes(id:String);
     public function getImage(id:String);
+    public function getFont(id:String);
     public function getAudio(id:String);
     public function getVideo(id:String);
-    public function getFont(id:String);
-
-    public function getFallbackText(id:String):String;
-    public function getFallbackBytes(id:String);
-    public function getFallbackImage(id:String);
-    public function getFallbackAudio(id:String);
-    public function getFallbackVideo(id:String);
-    public function getFallbackFont(id:String);
+    
+    //Synchronous asset fetch from default unmodified set:
+    public function getTextFallback(id:String):String;
+    public function getBytesFallback(id:String);
+    public function getFontFallback(id:String);
+    public function getImageFallback(id:String);
+    public function getAudioFallback(id:String);
+    public function getVideoFallback(id:String);
+    
+    /*
+    //Asynchronous asset fetch from modified set:
+    public function loadText(id:String):String;
+    public function loadBytes(id:String);
+    public function loadImage(id:String);
+    public function loadAudio(id:String);
+    public function loadVideo(id:String);
+    public function loadFont(id:String);
+    
+    //Asynchronous asset fetch from default unmodified set:
+    public function loadTextFallback(id:String);
+    public function loadBytesFallback(id:String);
+    public function loadFontFallback(id:String);
+    public function loadImageFallback(id:String);
+    public function loadAudioFallback(id:String);
+    public function loadVideoFallback(id:String);
+    */
 }
--- polymod/backends/OpenFLBackend.hx
+++ polymod/backends/OpenFLBackend.hx
@@ -40,7 +40,7 @@ class OpenFLBackend implements IBackend extends OpenFLAssetLibrary
     
     private var hasFallback:Bool = false;
 
-    function new(fallback:OpenFLAssetLibrary) 
+    function new (fallback:OpenFLAssetLibrary) 
     {
         #if !openfl
         throw "OpenFLBackend: needs the openfl library!";
@@ -50,37 +50,44 @@ class OpenFLBackend implements IBackend extends OpenFLAssetLibrary
         hasFallback = this.fallback != null;
     }
 
-    public function getText(id:String):String { return modLibrary.getText(id);}   //returns String
-    public function getBytes(id:String) { return modLibrary.getBytes(id); }       //returns lime.util.Bytes (abstract over Haxe Bytes)
-    public function getImage(id:String) { return modLibrary.getImage(id); }       //returns lime.graphics.Image
-    public function getFont(id:String){ return modLibrary.getFont(id); }          //returns lime.text.Font
-    public function getAudio(id:String) { return modLibrary.getAudioBuffer(id); } //returns lime.audio.AudioBuffer
-    public function getVideo(id:String) {
+    public function getText (id:String):String { return modLibrary.getText(id);}   //returns String
+    public function getBytes (id:String) { return modLibrary.getBytes(id); }       //returns lime.util.Bytes (abstract over Haxe Bytes)
+    public function getImage (id:String) { return modLibrary.getImage(id); }       //returns lime.graphics.Image
+    public function getFont (id:String)  { return modLibrary.getFont(id); }          //returns lime.text.Font
+    public function getAudio (id:String) { return modLibrary.getAudioBuffer(id); } //returns lime.audio.AudioBuffer
+    public function getVideo (id:String) 
+    {
         //should put a warning here probably 
         return getBytes(id);                                                      //video not supported as an asset type in OpenFL, just returns raw bytes
     }                  
         
-    public function getTextFallback(id:String):String { 
+    public function getTextFallback (id:String):String
+    {
         if(!hasFallback) return null;
         return fallback.getText(id);
     }
-    public function getBytesFallback(id:String) { 
+    public function getBytesFallback (id:String)
+    {
         if(!hasFallback) return null;
         return fallback.getBytes(id);
     }
-    public function getImageFallback(id:String) { 
+    public function getImageFallback (id:String)
+    {
         if(!hasFallback) return null;
         return fallback.getImage(id);
     }
-    public function getFontFallback(id:String) {
+    public function getFontFallback (id:String)
+    {
         if(!hasFallback) return null;
         return fallback.getFont(id);
     }
-    public function getAudioFallback(id:String) {
+    public function getAudioFallback (id:String)
+    {
         if(!hasFallback) return null;
-        return fallback.getAudioBuffer(id);
+        return fallback.getAudioBuffer (id);
     }
-    public function getVideoFallback(id:String) {
+    public function getVideoFallback (id:String)
+    {
         if(!hasFallback) return null;
         //should put a warning here probably 
         return fallback.getBytes(id);
