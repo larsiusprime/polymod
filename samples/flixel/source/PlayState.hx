3b6136f89c36d53dc9fa035b30e9d4d7c9166eb5
Eric Myllyoja <ericmyllyoja@gmail.com>
2022-01-30 04:19:59 -0500

    Started on a flixel sample.

---
+++ samples/flixel/source/PlayState.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	public override function create()
+	{
+		super.create();
+	}
+
+	public override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+
+	function loadMods() {}
+}
