e76deed9a2a38480bb074aa33f55002a38944fcf
UncertainProd <uncertainprod@github.com>
2022-08-15 01:41:25 +0530

    Added some zip-related utilities and finished up the zip-filesystem for sys targets

--- polymod/fs/SysZipFileSystem.hx
+++ polymod/fs/SysZipFileSystem.hx
@@ -1,59 +1,78 @@
 package polymod.fs;
 
+import haxe.io.Bytes;
+import haxe.io.Path;
+import polymod.Polymod.ModMetadata;
 import polymod.fs.MemoryZipFileSystem.ZipFileSystemParams;
+import polymod.util.Util;
+import thx.semver.VersionRule;
+#if sys
+import polymod.util.ZipParser;
+import sys.io.File;
+#end
 
+#if !sys
+class SysZipFileSystem extends StubFileSystem
+{
+	public function new(params:PolymodFileSystemParams)
+	{
+		super(params);
+		Polymod.warning(FUNCTIONALITY_NOT_IMPLEMENTED, "This file system not supported for this platform, and is only intended for use on sys targets");
+	}
+}
+#else
+/**
+ * An implementation of an IFileSystem that can access files from an un-compressed zip archive, for use on sys-targets
+ * Useful for loading mods from zip files.
+ * Currently does not support compressed zip files.
+ */
 class SysZipFileSystem extends MemoryFileSystem
 {
-    public function new(params:ZipFileSystemParams)
-    {
-        super(params);
-        var zipname = (params.zipName != null) params.zipName : "";
-        var zippath = (params.zipPath != null) params.zipPath : "";
-        initFileSystem(zippath, zipname);
-    }
+	var zipparser:ZipParser;
 
-	public override function exists(path:String):Bool
-    {
-        return null;
-    }
-    
-	public override function isDirectory(path:String):Bool
-    {
-        return false;
-    }
-
-	public override function readDirectory(path:String):Array<String>
-    {
-        return null;
-    }
-
-	public override function readDirectoryRecursive(path:String):Array<String>
-    {
-        return null;
-    }
+	public function new(params:ZipFileSystemParams)
+	{
+		super(params);
+		var zipname = (params.zipName != null) ? params.zipName : "";
+		var zippath = (params.zipPath != null) ? params.zipPath : "";
+		initFileSystem(zippath, zipname);
+	}
 
 	public override function getFileContent(path:String):Null<String>
-    {
-        return null;
-    }
+	{
+		var data:Bytes = getFileBytes(path);
+		if (data == null)
+		{
+			return null;
+		}
+		return data.toString();
+	}
 
 	public override function getFileBytes(path:String):Null<Bytes>
-    {
-        return null;
-    }
+	{
+		if (!files.exists(path))
+		{
+			return null;
+		}
+		// strip the leading 'mods/' part of the file
+		if (StringTools.startsWith(path, 'mods/'))
+			path = path.substring(5);
+		var lfh = zipparser.getLocalFileHeaderOf(path);
+		var data = lfh.readData();
+		return data;
+	}
 
-	public override function scanMods(?apiVersionRule:VersionRule):Array<ModMetadata>
-    {
-        return null;
-    }
-
-	public override function getMetadata(modId:String):Null<ModMetadata>
-    {
-        return null;
-    }
-
-    public function initFileSystem(zippath:String, zipname:String)
-    {
-        
-    }
+	public function initFileSystem(zippath:String, zipname:String)
+	{
+		zipparser = new ZipParser(zippath);
+		for (fname => cdfh in zipparser.centraldirrecords)
+		{
+			if (cdfh.compressedSize != 0 && cdfh.uncompressedSize != 0 && !StringTools.endsWith(cdfh.filename, '/'))
+			{
+				trace('adding $fname');
+				addFileBytes('mods/$fname', null); // doing this just so directories get filled up
+			}
+		}
+	}
 }
+#end
