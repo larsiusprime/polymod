993b20da4e1d483f01dff2c95132ecaf1d25cd48
Kolo <67389779+KoloInDaCrib@users.noreply.github.com>
2026-08-21 13:46:18 +0200

    Fix function overrides not using the old positions

--- polymod/hscript/_internal/PolymodBaseClassMacro.hx
+++ polymod/hscript/_internal/PolymodBaseClassMacro.hx
@@ -215,6 +215,7 @@ class PolymodBaseClassMacro
             doesReturnVoid = (f.ret.toString() == 'Void');
           }
 
+          var oldPos:haxe.macro.Expr.Position = f.expr.pos;
           f.expr = macro
             {
               if (_asc != null && !_skipAscFrom.contains($v{fields[i].name}))
@@ -239,6 +240,8 @@ class PolymodBaseClassMacro
               // Fallback, call the original function.
               ${f.expr}
             };
+
+          f.expr.pos = oldPos;
 
         default:
           // Do nothing.
